fea209aecb060f9578dadb76677dd8b071f2033e
Pedro M <membrep@gmail.com>
2019-10-14 17:04:41 -0700

    Completed Opposite Numbers

---
+++ opposite_numbers.js
@@ -0,0 +1,18 @@
+// Refactored Code
+function opposite(number) {
+  return -number;
+}
+
+// Using Logical and Comparison Operators
+// function opposite(number){
+//   if(number > 0 || number < 0){
+//     return number * -1;
+//   } else {
+//     return number;
+//   }
+// }
+
+console.log(opposite(-10));
+console.log(opposite(10));
+console.log(opposite(0));
+console.log(opposite(27.50));
