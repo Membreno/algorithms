deb3679cc3efbde3f4b47a061b3c58ec432cc068
Pedro M <membrep@gmail.com>
2019-10-18 10:32:53 -0700

    refactored code for even or odd

--- even_or_odd.js
+++ even_or_odd.js
@@ -1,9 +1,14 @@
 // Given a integer, determine if number is even or odd
-function even_or_odd(number) {
-  if(number % 2 === 0){
-    return "Number is Even"
-  }
-  return "Number is Odd"
+// function even_or_odd(number) {
+//   if(number % 2 === 0){
+//     return "Number is Even";
+//   }
+//   return "Number is Odd";
+// }
+
+// Using a ternary operator
+function even_or_odd(number){
+  return number % 2 === 0 ? "Even" : "Odd";
 }
 
 console.log(even_or_odd(4)); // EVEN
