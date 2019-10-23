5abd8edffe2c4c3bc974e141cb593a134e005083
Pedro M <membrep@gmail.com>
2019-10-22 22:34:38 -0700

    finished who likes algorithm

---
+++ who_likes.js
@@ -0,0 +1,24 @@
+// Given an array of names, containing a list of people who like an item, list users who like in the following format
+// likes [] "no one likes this"
+// likes ["Peter"] must be "Peter likes this"
+// likes ["Jacob", "Alex"] must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] must be "Max, John, and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] must be "Alex, Jacob and 2 others like this"
+
+function likes(names){
+  switch (names.length) {
+    case 0:
+      return `No one likes this.`;
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]}, and ${names[2]} like this`;
+  }
+  if (names.length > 3){
+    return `${names[0]}, ${names[1]}, and ${names.length - 2} others like this`;
+  }
+}
+
+console.log(likes(["Alex", "Jacob", "Mark", "Max", "Billy"]));
