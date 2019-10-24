a9bcada81fb982e34cefe3bea8eec8db0daab528
Pedro M <membrep@gmail.com>
2019-10-24 11:42:45 -0700

    completed highest and lowest algorithm

---
+++ highest_and_lowest.js
@@ -0,0 +1,22 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+function makeResult(max, min) {
+  let result = `${max} ${min}`
+  return result;
+}
+
+function highAndLow(string) {
+  let numbers = string.split(" ");
+  let max = numbers[0];
+  let min = numbers[0];
+
+  for (let i = 0; i < string.length; i++) {
+    if (Number(numbers[i]) < min) {
+      min = numbers[i];
+    }
+    if (Number(numbers[i]) > max) {
+      max = numbers[i];
+    }
+  }
+  return makeResult(max, min);
+}
