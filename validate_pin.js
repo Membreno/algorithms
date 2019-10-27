cf2d4c66afb88e70ae3c95e312818145567e715c
Pedro M <membrep@gmail.com>
2019-10-27 09:51:06 -0700

    completed validate pin algorithm

---
+++ validate_pin.js
@@ -0,0 +1,27 @@
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// If the function is passed a valid PIN string, return true, else return false.
+
+function validatePIN(pin) {
+  pin = pin.toString();
+  var numbers = /^[0-9]+$/;
+  let pinLength = pin.length;
+
+  if (pin.match(numbers) === null) {
+    return false;
+  }
+  if (isNaN(pin)) {
+    return false;
+  }
+  if (pinLength === 4 || pinLength === 6) {
+    return true;
+  }
+  return false;
+}
+
+
+// Test Code
+console.log(validatePIN("1243")); // True
+console.log(validatePIN("123")); // False
+console.log(validatePIN("ab12")); // False
+console.log(validatePIN("124355")); // True
+console.log(validatePIN('-1.234')); // False
