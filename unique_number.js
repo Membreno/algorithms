a3e32d8781f146c18687c9dead55a5422f355290
Pedro M <membrep@gmail.com>
2019-11-04 12:14:19 -0800

    added alternative solution to unique number algo

--- unique_number.js
+++ unique_number.js
@@ -1,4 +1,4 @@
-// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// There is an array with some numbers. All numbers are equal except for one. Try to find it! It’s guaranteed that array contains more than 3 numbers. The tests contain some very huge arrays, so think about performance.
 
 function findUniq(array) {
   let numCount = {};
@@ -12,3 +12,17 @@ function findUniq(array) {
 console.log(findUniq([0,0,1,0,0]));
 console.log(findUniq([1,1,0.55,1,1]));
 console.log(findUniq([1,0,0,0,0]));
+
+
+// Alternative Solution
+function findUniq2(array) {
+  let[a, b, c] = array.slice(0, 3);
+  if(a !== b && a !== c){
+    return a;
+  }
+  for(let x of array){
+    if (x !== a){
+      return x;
+    }
+  }
+}
